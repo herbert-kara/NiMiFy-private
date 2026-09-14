02323e485ccec7422861d843b3de2962a8e4b538
Nim4a <nim4a@users.noreply.github.com>
2026-09-14 23:26:37 +0330

    fix: missing home_bg_accent import (CI v4.2.0)

--- lib/features/home/widget/connection_button.dart
+++ lib/features/home/widget/connection_button.dart
@@ -10,6 +10,7 @@ import 'package:hiddify/features/connection/notifier/connection_notifier.dart';
 import 'package:hiddify/features/profile/notifier/active_profile_notifier.dart';
 import 'package:hiddify/features/proxy/active/active_proxy_notifier.dart';
 import 'package:hiddify/features/settings/notifier/config_option/config_option_notifier.dart';
+import 'package:hiddify/features/home/widget/home_bg_accent.dart';
 import 'package:hiddify/features/home/widget/switch_connection_button.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
